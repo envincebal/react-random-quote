e0f630aa429c83c4e14da0b203b5e87357867499
envincebal <vincentyan1@gmail.com>
2018-06-17 23:13:38 -0700

    moved import index.css to index.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Quote from "./components/Quote";
 import Buttons from "./components/Buttons";
-import './index.css';
 
 class App extends Component {
   constructor(props) {
